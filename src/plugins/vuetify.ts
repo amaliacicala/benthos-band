8880496d66a420d62a35ee9475616b9e1f0e2b11
Amalia Cicala <cicala.amalia@gmail.com>
2024-05-14 21:45:25 +0200

    💄 add colour palette

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -7,15 +7,22 @@ import type { ThemeDefinition } from 'vuetify'
 const lightTheme: ThemeDefinition = {
   dark: false,
   colors: {
-    white: '#FFFDFC',
-    black: '#252522',
-    background: '#FFFDFC',
-    primary: '#b2464e',
-    secondary: '#f18257',
-    tertiary: '#75ba86',
-    accent: '#cf6895',
-    error: '#E7004C',
-    'on-primary': '#FFFDFC'
+    white: '#f4f2e4',
+    black: '#000000',
+    'green-darken-4': '#312f28',
+    'green-darken-3': '#343221',
+    'green-lighten-2': '#aab5a1',
+    'green-lighten-1': '#bec3b3',
+    'teal-darken-4': '#546061',
+    'teal-darken-2': '#809290',
+    'brown-lighten-5': '#f6f2de',
+    'brown-lighten-4': '#ab9164',
+    'brown-darken-4': '#282423',
+    'brown-darken-3': '#734427',
+    'brown-darken-2': '#411e17',
+    'amber-darken-1': '#e8b72b',
+    'purple-lighten-1': '#956c8b',
+    'pink-lighten-4': '#c09a9d'
   },
   variables: {
     'high-emphasis-opacity': 1,
