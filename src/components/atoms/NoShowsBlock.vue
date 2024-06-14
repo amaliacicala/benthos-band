be742d6521256b4c63d2d93367504feebe73ceae
Amalia Cicala <cicala.amalia@gmail.com>
2024-06-14 18:31:17 +0200

    💄 improve NoShowsBlock style for small screen

--- src/components/atoms/NoShowsBlock.vue
+++ src/components/atoms/NoShowsBlock.vue
@@ -11,11 +11,13 @@ const props = defineProps({
 
 <template>
   <div
-    class="d-flex flex-column align-self-center justify-center align-center w-75 pa-8"
+    class="d-flex flex-column align-self-center justify-center align-center text-center w-100 w-lg-75 pa-8"
     style="border: solid 1px #343221; border-radius: 8px"
   >
     <p class="text-h3 text-green-darken-3">Sorry, no upcoming shows for the moment.</p>
-    <p class="text-body-1 mt-2">But you can be the first to know when we're back on stage.</p>
+    <p class="text-body-1 mt-6 mt-md-2">
+      But you can be the first to know when we're back on stage.
+    </p>
     <v-btn
       :href="props.notifyLink"
       target="_blank"
