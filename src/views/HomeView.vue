d87b4f334dfcd2206f8263d37b05bad9cd73ffe8
Amalia Cicala <cicala.amalia@gmail.com>
2024-10-23 23:46:25 +0200

    🔥 remove newsletter section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,13 +19,4 @@ import TourDatesSection from '@/components/sections/TourDatesSection.vue'
   <MerchSection />
   <MusicSection />
   <TourDatesSection :show-header="true" class="bg-paper-green" />
-
-  <div class="d-flex align-center justify-center" :style="{ height: '300px' }">
-    <iframe
-      src="https://bandsintown.com/artist/1419641/email_signup_form?headerTextColor=rgba(244,242,228,1)&backgroundColor=transparent&ctaBackgroundColor=rgba(244,242,228,1)&title=Newsletter&headerTextStyle=normal&headerText=Sign up to get the latest Benthos updates.&font=JetBrains Mono&ctaIcon=show&ctaBorderRadius=4px&ctaBorderWidth=2px&ctaBorderColor=rgba(151,50,61,1)&ctaFontColor=rgba(151,50,61,1)&alignment=center&emailInputField=show&ctaLabel=Subscribe&layout=wide&locale=en&ctaSize=medium"
-      width="100%"
-      height="100%"
-      title="newsletter-widget"
-    ></iframe>
-  </div>
 </template>
